e5a18b3dbb41b36a3086f4d1b8c5bf0dfb846589
zille-ali <41956489+zille-ali@users.noreply.github.com>
2018-07-31 18:28:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require ("express");
 var app = express();
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3000;
 app.get("/", function(req,res){
   res.send("Hello World");
   });
